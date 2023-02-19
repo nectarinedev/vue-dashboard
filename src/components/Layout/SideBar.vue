c83aa01cab73f89a502df24f36dfddf6b6d168ce
chrissayon <christopher.l.sayon@gmail.com>
2023-02-20 00:37:33 +1100

    prettier formatting

--- src/components/Layout/SideBar.vue
+++ src/components/Layout/SideBar.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
-
 interface SidebarList {
 	name: string;
 	link: string;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import OverviewView from '@/views/OverviewView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import OverviewView from '@/views/OverviewView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'overview',
-      component: OverviewView
-    },
-    {
-      path: '/trafficsource',
-      name: 'trafficsource',
-      component: () => import('@/views/TrafficSourceVIew.vue')
-    }
-  ]
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'overview',
+			component: OverviewView,
+		},
+		{
+			path: '/trafficsource',
+			name: 'trafficsource',
+			component: () => import('@/views/TrafficSourceVIew.vue'),
+		},
+	],
+});
 
-export default router
+export default router;
